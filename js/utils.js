d1b7a1ae6ac46a768fb9c890c6616c723c1ef432
Angel00705 <angeltar0070520@gmail.com>
2025-11-09 17:19:25 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,12 @@
-// utils.js - minimal version
-console.log('utils.js loaded');
+// utils.js - minimal working version
+class Utils {
+    static formatDate(date) {
+        return new Date(date).toLocaleDateString('ru-RU');
+    }
+    
+    static formatCurrency(amount) {
+        return amount + ' ₽';
+    }
+}
+
+window.Utils = Utils;
