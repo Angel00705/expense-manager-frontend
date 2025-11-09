8b6e811e7c0f083f76cd127aa634f6c2f69a998b
Angel00705 <angeltar0070520@gmail.com>
2025-11-09 17:19:09 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,2 +1,18 @@
-// auth.js - minimal version
-console.log('auth.js loaded');
+// auth.js - minimal working version
+class Auth {
+    static login(email, password) {
+        console.log('Login attempt:', email);
+        return Promise.resolve(true);
+    }
+    
+    static logout() {
+        localStorage.removeItem('token');
+        window.location.href = 'login.html';
+    }
+    
+    static isLoggedIn() {
+        return !!localStorage.getItem('token');
+    }
+}
+
+window.Auth = Auth;
